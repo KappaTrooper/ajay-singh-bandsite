d48d870759419ddc0eedf3438e6da8b27359e75c
KappaTrooper <contact@ajaysingh.ca>
2023-05-11 22:16:06 -0400

    Fixed some styling issues, working on changing position: absolute; to flex box, API Key is now and successfully getting data back from API and able to view comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,83 +1,15 @@
-let comment = [
-      
-      {
-        "name": "Connor Walton",
-        "date": "02/17/2021",
-        "comment": "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-      },
-
-  
-      {
-        "name": "Emilie Beach",
-        "date": "01/09/2021",
-        "comment": "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-      },
-
-      {
-        "name": "Miles Acosta",
-        "date": "12/20/2020",
-        "comment": "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-      },
-]
-
-
+// API Key DO not Deletus, get new key with "com/register"
+const commentUrl = "https://project-1-api.herokuapp.com/comments/?api_key=a0f613e9-9a3f-4cbf-ab82-c0cc9a258ef4";
 const form = document.querySelector('.form');
 const commentSection = document.querySelector('.comment-section');
 
+// Axios getting data with then
+axios.get(commentUrl).then((response) => {
+  const comments = response.data;
 
+    // create elements  -------------------- start -------------------- 
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  
-  const nameInput = event.target.querySelector('input[name="commentName"]');
-  const commentInput = event.target.querySelector('textarea[name="bio"]');
-  
-  if (!nameInput || !commentInput) {
-    return; // undefiened 
-  }
-  
-  const name = nameInput.value;
-  const commentText = commentInput.value;
-  
-  // alert section -------------------- start -------------------- 
-  if (!name || !commentText) {
-    alert('Please enter both your name and comment.'); 
-    return; 
-  }
-
-  // alert section -------------------- Ends -------------------- 
-  
-
-  // local tim
-  const date = new Date().toLocaleDateString(); 
-  
-  const newComment = {
-    name: name,
-    comment: commentText,
-    date: date
-  };
-  
-  // array [0]
-  comment.unshift(newComment); 
-  
-  // clear form
-  nameInput.value = '';
-  commentInput.value = '';
-  
-  updateComments();
-});
-
-function updateComments() {
-  
-  while (commentSection.firstChild) {
-    // removing first child
-    commentSection.removeChild(commentSection.firstChild);
-    
-  }
- 
-  // create elements  -------------------- start -------------------- 
-  
-  comment.forEach((item) => {
+  comments.forEach((item) => {
     const hr = document.createElement('hr');
     const commentItem = document.createElement('section');
     const avatarDiv = document.createElement('div');
@@ -91,11 +23,11 @@ function updateComments() {
     const bottomDiv = document.createElement('div');
     const commentSpan = document.createElement('span');
 
-
-  //create element -------------------- end -------------------- 
+     //create element -------------------- end -------------------- 
 
 
   // -------------------- class name
+
     hr.className = 'comment-divider';
     commentItem.className = 'comment-item';
     avatarDiv.className = 'comment-item__avatar';
@@ -113,11 +45,13 @@ function updateComments() {
     avatarImg.src = 'assets/Images/grey.jpeg';
     avatarImg.alt = 'Avatar';
     nameSpan.innerHTML = item.name;
-    dateSpan.innerHTML = item.date;
+    
+    // converting timestamp to localtime
+    const date = new Date(item.timestamp);
+    dateSpan.innerHTML = date.toLocaleDateString();
+    
     commentSpan.innerHTML = item.comment;
 
-
-    
     nameDiv.appendChild(nameSpan);
     dateDiv.appendChild(dateSpan);
     topDiv.appendChild(nameDiv);
@@ -126,12 +60,13 @@ function updateComments() {
     commentDiv.appendChild(bottomDiv);
     bottomDiv.appendChild(commentSpan);
     avatarDiv.appendChild(avatarImg);
+
     commentItem.appendChild(avatarDiv);
     commentItem.appendChild(commentDiv);
     commentSection.appendChild(hr);
     commentSection.appendChild(commentItem);
+    commentDiv.appendChild(topDiv);
+    commentDiv.appendChild(bottomDiv);
+    bottomDiv.appendChild(commentSpan);
   });
-}
-
-
-updateComments();
+});
