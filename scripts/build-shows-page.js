7f689e5a30344ed6e737a14d007310a1e141614c
KappaTrooper <contact@ajaysingh.ca>
2023-05-14 23:31:03 -0400

    Finished styling and added new comment section

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,100 +1,167 @@
-showsUrl =
-  "https://project-1-api.herokuapp.com/showdates?api_key=d141a6e3-78bf-4498-9e93-ae35bb59ac9e";
 
-const showSection = document.querySelector(".show-section");
+showsUrl = "https://project-1-api.herokuapp.com/showdates?api_key=d8f8b182-f03a-4728-8cb0-4ee5d8f0c4aa";
 
-const showTitle = document.createElement("h3");
-showTitle.classList.add("show-title");
-showTitle.textContent = "Shows";
 
-const ShowBoxContainer = showSection.querySelector(".show-box");
+const showSection = document.querySelector('.show-section'); 
+
+const showTitle = document.createElement('h3');
+showTitle.classList.add('show-title');
+showTitle.textContent = 'Shows';
+
+const ShowBoxContainer = showSection.querySelector('.show-box');
 showSection.insertBefore(showTitle, ShowBoxContainer);
 
+
+
+
+
+
+
+
+
+
+
+
 let FirstHeader = true;
+
+
+
 
 // Axios Promise Get Shows data
 axios.get(showsUrl).then((response) => {
   const shows = response.data;
 
-  // --------------------  loop start --------------------
-  shows.forEach((show) => {
-    const showBox = document.createElement("div");
-    showBox.classList.add("show-box");
 
-    // adding show-box__top when FirstHeader = false
-    if (FirstHeader) {
-      showBox.classList.add("show-box__top");
-      FirstHeader = false;
-    }
 
-    showBox.addEventListener("click", function () {
-      const showBoxes = document.querySelectorAll(".show-box");
-      // adds selected stated
-      showBoxes.forEach((sbox) => {
-        sbox.classList.remove("active");
-      });
 
-      showBox.classList.add("active");
+// --------------------  loop start -------------------- 
+shows.forEach(show => {
+
+  
+  const showBox = document.createElement('div');
+  showBox.classList.add('show-box');
+
+  // adding show-box__top when FirstHeader = false
+  if (FirstHeader) {
+    showBox.classList.add('show-box__top');
+    FirstHeader = false;
+  }
+
+  showBox.addEventListener('click', function() {
+    
+    const showBoxes = document.querySelectorAll('.show-box');
+// adds selected stated 
+    showBoxes.forEach(sbox => {
+      sbox.classList.remove('active');
     });
 
-    showBox.addEventListener("mouseover", function () {
-      showBox.classList.add(".hover");
-    });
-
-    //-------------------- elements for date, venue, and location --------------------
-    const dateTitle = document.createElement("div");
-    dateTitle.classList.add("show-box__title");
-    const dateHeader = document.createElement("p");
-    dateHeader.classList.add("show-box__header");
-
-    dateHeader.innerHTML = "Date";
-    const dateText = document.createElement("h3");
-    dateText.classList.add("show-box__date");
-
-    const dateShow = new Date(show.date);
-
-    const realTime = dateShow.toLocaleDateString();
-
-    dateText.innerHTML = realTime;
-    dateTitle.appendChild(dateHeader);
-    dateTitle.appendChild(dateText);
-
-    const venueTitle = document.createElement("div");
-    venueTitle.classList.add("show-box__title");
-    const venueHeader = document.createElement("p");
-    venueHeader.classList.add("show-box__header");
-    venueHeader.innerHTML = "Venue";
-    const venueText = document.createElement("h3");
-    venueText.classList.add("show-box__venue");
-    venueText.innerHTML = show.place;
-    venueTitle.appendChild(venueHeader);
-    venueTitle.appendChild(venueText);
-
-    const locationTitle = document.createElement("div");
-    locationTitle.classList.add("show-box__title");
-    const locationHeader = document.createElement("p");
-    locationHeader.classList.add("show-box__header");
-    locationHeader.innerHTML = "Location";
-    const locationText = document.createElement("h3");
-    locationText.classList.add("show-box__location");
-    locationText.innerHTML = show.location;
-    locationTitle.appendChild(locationHeader);
-    locationTitle.appendChild(locationText);
-
-    // --------------------  Button creation start --------------------
-    const buyTicketButton = document.createElement("input");
-    buyTicketButton.setAttribute("type", "submit");
-    buyTicketButton.classList.add("show-box__submit");
-    buyTicketButton.setAttribute("value", "Buy Ticket");
-
-    // --------------------  attachin tutle and button to showBox div --------------------
-
-    showBox.appendChild(dateTitle);
-    showBox.appendChild(venueTitle);
-    showBox.appendChild(locationTitle);
-    showBox.appendChild(buyTicketButton);
-
-    // Append the show box element to the parent element
-    showSection.appendChild(showBox);
+    showBox.classList.add('active');
   });
+
+  showBox.addEventListener('mouseover', function() { 
+    showBox.classList.add('.hover')
+
+  });
+
+  
+
+
+
+
+
+
+
+
+  //-------------------- elements for date, venue, and location -------------------- 
+  const dateTitle = document.createElement('div');
+  dateTitle.classList.add('show-box__title');
+  const dateHeader = document.createElement('p');
+  dateHeader.classList.add('show-box__header');
+
+
+  dateHeader.innerHTML = 'Date';
+  const dateText = document.createElement('h3');
+  dateText.classList.add('show-box__date');
+
+  const dateShow = new Date(show.date);
+
+  // -- converting timestamp to normal readable time ------
+
+  const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+  const realTime = dateShow.toLocaleDateString(undefined, options);
+  dateText.innerHTML = realTime;
+
+
+  dateText.innerHTML = realTime;
+  dateTitle.appendChild(dateHeader);
+  dateTitle.appendChild(dateText);
+
+  const venueTitle = document.createElement('div');
+  venueTitle.classList.add('show-box__title');
+  const venueHeader = document.createElement('p');
+  venueHeader.classList.add('show-box__header');
+  venueHeader.innerHTML = 'Venue';
+  const venueText = document.createElement('h3');
+  venueText.classList.add('show-box__venue');
+  venueText.innerHTML = show.place;
+  venueTitle.appendChild(venueHeader);
+  venueTitle.appendChild(venueText);
+
+  const locationTitle = document.createElement('div');
+  locationTitle.classList.add('show-box__title');
+  const locationHeader = document.createElement('p');
+  locationHeader.classList.add('show-box__header');
+  locationHeader.innerHTML = 'Location';
+  const locationText = document.createElement('h3');
+  locationText.classList.add('show-box__location');
+  locationText.innerHTML = show.location;
+  locationTitle.appendChild(locationHeader);
+  locationTitle.appendChild(locationText);
+
+
+  
+
+
+
+
+
+  // --------------------  Button creation start -------------------- 
+  const buyTicketButton = document.createElement('input');
+  buyTicketButton.setAttribute('type', 'submit');
+  buyTicketButton.classList.add('show-box__submit');
+  buyTicketButton.setAttribute('value', 'Buy Ticket');
+
+
+  // --------------------  attachin tutle and button to showBox div -------------------- 
+  
+  showBox.appendChild(dateTitle);
+  showBox.appendChild(venueTitle);
+  showBox.appendChild(locationTitle);
+  showBox.appendChild(buyTicketButton);
+
+    
+
+  // Append the show box element to the parent element
+  showSection.appendChild(showBox);
+
+  
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 });
+});
+
+
+
+
+
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,72 +1,131 @@
+
 // API Key DO not Deletus, get new key with "com/register"
-const commentUrl = "https://project-1-api.herokuapp.com/comments/?api_key=a0f613e9-9a3f-4cbf-ab82-c0cc9a258ef4";
+const commentUrl = "https://project-1-api.herokuapp.com/comments?api_key=56a5e96d-0610-400e-8284-b0f71dce3b19";
 const form = document.querySelector('.form');
 const commentSection = document.querySelector('.comment-section');
 
-// Axios getting data with then
-axios.get(commentUrl).then((response) => {
-  const comments = response.data;
-
     // create elements  -------------------- start -------------------- 
-
-  comments.forEach((item) => {
-    const hr = document.createElement('hr');
-    const commentItem = document.createElement('section');
-    const avatarDiv = document.createElement('div');
-    const avatarImg = document.createElement('img');
-    const commentDiv = document.createElement('div');
-    const topDiv = document.createElement('div');
-    const nameDiv = document.createElement('div');
-    const nameSpan = document.createElement('span');
-    const dateDiv = document.createElement('div');
-    const dateSpan = document.createElement('span');
-    const bottomDiv = document.createElement('div');
-    const commentSpan = document.createElement('span');
-
+function createCommentElement(item) {
+  const hr = document.createElement('hr');
+  const commentItem = document.createElement('section');
+  const avatarDiv = document.createElement('div');
+  const avatarImg = document.createElement('img');
+  const commentDiv = document.createElement('div');
+  const topDiv = document.createElement('div');
+  const nameDiv = document.createElement('div');
+  const nameSpan = document.createElement('span');
+  const dateDiv = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const bottomDiv = document.createElement('div');
+  const commentSpan = document.createElement('span');
      //create element -------------------- end -------------------- 
 
-
   // -------------------- class name
+  hr.className = 'comment-divider';
+  commentItem.className = 'comment-item';
+  avatarDiv.className = 'comment-item__avatar';
+  avatarImg.className = 'comment-item__avatarimg';
+  commentDiv.className = 'comment-item__comment';
+  topDiv.className = 'comment-item__top';
+  nameDiv.className = 'comment-item__divname';
+  nameSpan.className = 'comment-item__name';
+  dateDiv.className = 'comment-item__divdate';
+  dateSpan.className = 'comment-item__date';
+  bottomDiv.className = 'comment-item__bottom';
+  commentSpan.className = 'comment-item__comment';
 
-    hr.className = 'comment-divider';
-    commentItem.className = 'comment-item';
-    avatarDiv.className = 'comment-item__avatar';
-    avatarImg.className = 'comment-item__avatarimg';
-    commentDiv.className = 'comment-item__comment';
-    topDiv.className = 'comment-item__top';
-    nameDiv.className = 'comment-item__divname';
-    nameSpan.className = 'comment-item__name';
-    dateDiv.className = 'comment-item__divdate';
-    dateSpan.className = 'comment-item__date';
-    bottomDiv.className = 'comment-item__bottom';
-    commentSpan.className = 'comment-item__comment';
-    
-    // grey image 
-    avatarImg.src = 'assets/Images/grey.jpeg';
-    avatarImg.alt = 'Avatar';
-    nameSpan.innerHTML = item.name;
-    
+    // grey image / name / comment
+  avatarImg.src = 'assets/Images/grey.jpeg';
+  avatarImg.alt = 'Avatar';
+  nameSpan.innerHTML = item.name;
+  commentSpan.innerHTML = item.comment;
     // converting timestamp to localtime
-    const date = new Date(item.timestamp);
-    dateSpan.innerHTML = date.toLocaleDateString();
-    
-    commentSpan.innerHTML = item.comment;
 
-    nameDiv.appendChild(nameSpan);
-    dateDiv.appendChild(dateSpan);
-    topDiv.appendChild(nameDiv);
-    topDiv.appendChild(dateDiv);
-    commentDiv.appendChild(topDiv);
-    commentDiv.appendChild(bottomDiv);
-    bottomDiv.appendChild(commentSpan);
-    avatarDiv.appendChild(avatarImg);
+  let date = new Date(item.timestamp);
+  dateSpan.innerHTML = date.toLocaleDateString();
 
-    commentItem.appendChild(avatarDiv);
-    commentItem.appendChild(commentDiv);
-    commentSection.appendChild(hr);
-    commentSection.appendChild(commentItem);
-    commentDiv.appendChild(topDiv);
-    commentDiv.appendChild(bottomDiv);
-    bottomDiv.appendChild(commentSpan);
-  });
-});
+  // -------------Adding name and date to divs (top and bottom)------
+  nameDiv.appendChild(nameSpan);
+  dateDiv.appendChild(dateSpan);
+  topDiv.appendChild(nameDiv);
+  topDiv.appendChild(dateDiv);
+  commentDiv.appendChild(topDiv);
+  commentDiv.appendChild(bottomDiv);
+  bottomDiv.appendChild(commentSpan);
+  avatarDiv.appendChild(avatarImg);
+
+
+  commentItem.appendChild(avatarDiv);
+  commentItem.appendChild(commentDiv);
+  commentSection.appendChild(hr);
+  commentSection.appendChild(commentItem);
+
+  
+  
+  
+}
+
+
+// Getting api data
+function getAPIComment() {
+  axios.get(commentUrl)
+    .then((response) => {
+      const comments = response.data;
+
+      // Sort array by data recenty date -> oldest comments
+      comments.sort((a, b) => b.timestamp - a.timestamp);
+
+      comments.forEach((item) => {
+        createCommentElement(item);
+      });
+    })
+  
+}
+
+
+
+// --------------form submission--------------
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const nameInput = document.querySelector('input[name="commentName"]');
+  const commentInput = document.querySelector('textarea[name="bio"]');
+
+  // Form Validation
+  if (nameInput.value.trim() === '' || commentInput.value.trim() === '') {
+    console.log('Please fill in both name and comment fields');
+    return;
+  }
+
+  const newComment = {
+    name: nameInput.value,
+    comment: commentInput.value,
+  };
+
+  // --------- getting new comment-----------------
+  axios.post(commentUrl, newComment, {
+    RequiredRequestHeaders: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      const item = response.data;
+      createCommentElement(item);
+
+      // ---- " " clears form values
+      nameInput.value = '';
+      commentInput.value = '';
+
+      // takes last comment added and putts it to the top 
+      const firstComment = commentSection.firstChild;
+      commentSection.insertBefore(commentSection.lastChild, firstComment);
+    })
+   
+}
+
+
+// Add event listener to form submit event
+form.addEventListener('submit', handleFormSubmit);
+
+// Fetch and display existing comments when the page loads
+getAPIComment();
